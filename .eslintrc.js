d7213117888d10f057f70df22430d1adf7cdaf51
Jean-Baptiste DEMONTE <jbdemonte@gmail.com>
2020-05-17 22:09:39 +0200

    Reduce filename

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es6": true
+        "es6": true,
+        "jest/globals": true
     },
     "extends": [
         "plugin:react/recommended",
@@ -20,7 +21,8 @@ module.exports = {
         "sourceType": "module"
     },
     "plugins": [
-        "react"
+        "react",
+        "jest"
     ],
     "rules": {
         "no-restricted-syntax": 0,
--- app/components/FileItem.jsx
+++ app/components/FileItem.jsx
@@ -3,18 +3,18 @@ import PropTypes from 'prop-types';
 import { statSync } from 'fs';
 import { basename } from 'path';
 import { FolderOutlined, FileOutlined } from '@ant-design/icons';
+import reduceFilename from '../tools/reduce-filename';
 
 const FileItem = ({ filePath, onFolderDoubleClick }) => {
   const stat = statSync(filePath);
   const isDirectory = stat.isDirectory();
   const onDoubleClick = isDirectory ? () => onFolderDoubleClick(filePath) : () => null;
 
-  // todo: basename => [20 on left] ... [5 on right] . extension
   return (
     <div className="file-item" onDoubleClick={onDoubleClick}>
       <div className="inner">
         { isDirectory ? <FolderOutlined /> : <FileOutlined />}
-        <span>{ basename(filePath) }</span>
+        <span>{ reduceFilename(basename(filePath)) }</span>
       </div>
     </div>
   );
